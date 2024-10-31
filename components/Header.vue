f5cb431df86009d2a430d59b85382918eb621d2b
alsyaaa <142280309+alsyanvma@users.noreply.github.com>
2024-10-31 08:50:40 +0700

    Add files via upload

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="row align-items-center p-3 bg-plum text-white">
     <div class="col-3 col-md-2 col-lg-1 text-center">
-      <img src="assets/img/LOGO-SMKN4.png" alt="logo SMKN 4" class="img-fluid logo-image" style="max-width: 100px;">
+      <picture>
+        <img 
+          src="assets/img/LOGO-SMKN4.png" 
+          alt="logo SMKN 4" 
+          class="img-fluid logo-image" 
+          style="max-width: 130px;" 
+          width="130" 
+          height="130">
+      </picture>
     </div>
     <div class="col-9 col-md-10 col-lg-11 text-container">
       <h1 class="header-title">Presensi Siswa</h1>
@@ -18,14 +26,6 @@
   background-color: #58A86A;
 }
 
-.header-title {
-  font-size: 24px;
-}
-
-.header-address {
-  font-size: 16px;
-}
-
 @media (max-width: 576px) {
   .text-container {
     margin-left: 10px;
@@ -36,17 +36,10 @@
   .header-title {
     font-size: 32px;
   }
-}
   .header-address {
     font-size: 20px;
-}
-
-@media (min-width: 992px) {
-  .header-title {
-    font-size: 40px;
   }
 }
-  .header-address {
-    font-size: 24px;
-}
+
+
 </style>
